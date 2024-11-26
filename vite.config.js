bf50d0d3fac0e03659a0f53cf9ff663d5b978831
Abdulla Vora <143266743+AbdullaVora@users.noreply.github.com>
2024-11-26 10:26:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces (required for Render)
-    port: process.env.PORT || 5173, // Use PORT environment variable or fallback to 5173
   },
 })
