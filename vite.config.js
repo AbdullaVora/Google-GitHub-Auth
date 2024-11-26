158fdccc8980945d6d59ecb31593ba651d2b8c53
Abdulla Vora <143266743+AbdullaVora@users.noreply.github.com>
2024-11-26 10:13:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces (required for Render)
+    port: process.env.PORT || 5173, // Use PORT environment variable or fallback to 5173
+  },
 })
